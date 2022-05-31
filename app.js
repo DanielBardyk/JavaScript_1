bade360b3122e77b2d682b34cbe8335bf3cec2eb
DanielBardyk <you@github.daniel31.com>
2022-05-31 22:49:05 +0300

    second commit

--- app.js
+++ app.js
@@ -1,82 +1,31 @@
-const startBtn = document.querySelector('#start')
-const screens = document.querySelectorAll('.screen')
-const timeList = document.querySelector('#time-list')
-const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
 const colors = ['#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3']
-let time = 0
-let score = 0
-startBtn.addEventListener('click', (event) => {
-    event.preventDefault()
-    screens[0].classList.add('up')
-})
+const SQUARES_NUMBER = 687
 
-timeList.addEventListener('click', event => 
-{
-    if (event.target.classList.contains('time-btn')) {
-        time = parseInt(event.target.getAttribute('data-time'))
-        screens[1].classList.add('up')
-        startGame()
-    }
-})
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
 
-board.addEventListener('click', event => {
-    if(event.target.classList.contains('circle')) {
-        score++
-        event.target.remove()
-        createRandomCircle()
-    }
-})
+    square.addEventListener('mouseover', () => setColor(square))
 
-function startGame() {
-    setInterval(decreaseTime, 1000)
-    createRandomCircle()
-    setTime(time)
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+
 }
 
-function decreaseTime() {
-    if (time===0) {
-        finishGame()
-    } else {
-        let current = --time
-        if (current < 10) {
-        current = `0${current}`
-    }
-    setTime(current)
-    }
-}
-
-function setTime(value) {
-    timeEl.innerHTML = `00:${value}`
-}
-
-function finishGame() {
-    timeEl.parentNode.classList.add('hide')
-    board.innerHTML = `<h1>Cчет: <span class='primary'>${score}</span></h1>`
-}
-
-function createRandomCircle() {
-    const circle = document.createElement('div')
-    const size = getRandomNumber(10, 60)
-    const {width, height} = board.getBoundingClientRect()
-    const x = getRandomNumber(0, width - size)
-    const y = getRandomNumber(0, height - size)
+function setColor(element) {
     const color = getRandomColor()
-    
-    circle.classList.add('circle')
-    circle.style.width = `${size}px`
-    circle.style.height = `${size}px`
-    circle.style.top = `${y}px`
-    circle.style.left = `${x}px`
-    circle.style.background = color
-    
-    board.append(circle)
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min)
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
 }
 
 function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)]
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
 }
